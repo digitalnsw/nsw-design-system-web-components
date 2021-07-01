dedfc7a39737778771b265889de58a456b046eb5
Rod Higgins <rod.higgins@senuamedia.com>
2021-07-01 11:00:03 +1000

    Added localised stotybook ultities

--- elements/nsw-blockquote/nsw-blockquote.stories.js
+++ elements/nsw-blockquote/nsw-blockquote.stories.js
@@ -5,7 +5,7 @@ import {
   text,
   boolean,
 } from "@open-wc/demoing-storybook";
-import { StorybookUtilities } from "@lrnwebcomponents/storybook-utilities/storybook-utilities.js";
+import { StorybookUtilities } from "@digitalnsw/storybook-utilities/storybook-utilities.js";
 import { NswBlockquote } from "./nsw-blockquote.js";
 // need to account for polymer goofiness when webpack rolls this up
 
